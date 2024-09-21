69d5e0dfd9b5f9d41bd07809e288625242b036d6
Lazer Sharp <email2barun@gmail.com>
2024-09-22 04:18:30 +0530

    Move cursor

--- pad.c
+++ pad.c
@@ -12,6 +12,7 @@
 
 /*** data ***/
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -139,7 +140,14 @@ void editorRefreshScreen() {
 
   editorDrawRows(&ab); // draw `~` chars
 
-  abAppend(&ab, "\x1b[H", 3);   // move cursor to the top
+  char buf[32];
+  snprintf(buf, sizeof(buf), "%d:%d", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+  
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+
+  // abAppend(&ab, "\x1b[H", 3);   // move cursor to the top
   abAppend(&ab, "\x1b[?25h", 6);// show  cursor 
 
   write(STDIN_FILENO, ab.buf, ab.len);
@@ -148,6 +156,23 @@ void editorRefreshScreen() {
 }
 
 /*** input ***/
+
+void editorMoveCursor(char key) {
+  switch (key) {
+    case 'j':
+      E.cy++;
+      break;
+    case 'k':
+      E.cy--;
+      break;
+    case 'h':
+      E.cx--;
+      break;
+    case 'l':
+      E.cx++;
+      break;
+  }
+}
 
 void editorProcessKeypress() {
   char c = editorReadKey();
@@ -159,11 +184,22 @@ void editorProcessKeypress() {
       exit(0);
       break;
     }
+
+    case 'j':
+    case 'k':
+    case 'h':
+    case 'l':
+      editorMoveCursor(c);
+      break;
   }
 }
 
 /*** init ***/
+
 void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+
   if(getWindowSize(&E.screenrows, &E.screencols) == -1) {
     die("getWindowSize");
   }
